4084705f01dc1f40ef9f1b84a54bf99a6a5b91c4
yashu77790 <130728835+yashu77790@users.noreply.github.com>
2026-02-06 18:05:35 +0530

    Add GreenGrid India SPA prototype

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
